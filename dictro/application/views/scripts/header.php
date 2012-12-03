9c020ad823185353e25491efe8f38695259866fb
swimmer <pape.pula@googlemail.com>
2012-12-03 10:17:25 +0100

    header

--- dictro/application/views/scripts/header.php
+++ dictro/application/views/scripts/header.php
@@ -6,18 +6,21 @@
 	</style>
 	</head>
 	<body>
-		<div class="imgleft" style="float: left; width: 600px; height: 200px;"> 
+	
+	
+		<div class="imgleft" style="float: left; width: 600px; height: 150;"> 
 			<img src="/../../../images/pic1.jpg" alt="eine grafik">
 		</div>
 		
-		<div class="imgmiddle" style="float: left;">
+		<div class="imgmiddle" style="float: left; width: 200px; height: 150px;">
 		<img src="/../../../images/dictro.jpg" alt="eine grafik">
 		</div>
 		
-		<div class="imgright" style="float: right;">
+		<div class="imgright" style="float: right; width: 200px; height: 150px;">
 			<a href="/home.php">German</a>
 			<a href="/home.php">English</a>
 		</div>
+	
 	<br>
 	<br>
 	</body>
