d56a101e1dfbc591820f1a2a341559371840a500
swimmer <pape.pula@googlemail.com>
2012-12-10 14:41:18 +0100

    contact

--- dictro/application/views/scripts/contact.php
+++ dictro/application/views/scripts/contact.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 <html>
@@ -61,8 +60,8 @@
 			<br>
 			<div id="blubb">
 				<form action="email.php" method="POST">
-		
-				
+					
+					
 						<div class="form" style="float:right; width:600px">
 						<dl>
 							<dt>Name:</dt>
@@ -78,7 +77,7 @@
 						<div class="send" style="float:right; width:330px">
 						<input type="submit" value="Senden" />
 						<input type="reset" value="Zurücksetzen" />
-						
+					 
 						</div>
 					</div>
 			
@@ -101,55 +100,3 @@
 	<br>
 	</body>
 	
-=======
-<html>
-<head>
-</head>
-<body>
-<table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="$border">
-  <tr bgcolor="green">
-    <td class="title">Contact</td>
-  </tr>
-  <tr><td bgcolor="green"></td></tr>
-  <tr bgcolor="green">
-    <td>
-    <table width="100%" border="0" cellspacing="0" cellpadding="2">
-      <tr>
-               <div align="center">
-        </div>
-        <form action="index.php?site=contact" method="post">
-        <input type="hidden" name="action" value="send" />
-        <table width="75%" border="0" cellspacing="0" cellpadding="2" align="center">
-          <tr>
-            <td>Name</td>
-            <td><input name="name" value="$name" size="40" /></td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td><input name="from" value="$from" size="40" /></td>
-          </tr>
-          <tr>
-            <td>Subject</td>
-            <td><input name="subject" value="$subject" size="40" /></td>
-          </tr>
-          <tr>
-            <td>Message</td>
-            <td><textarea name="text" cols="50" rows="6" style="width:99%;">$text</textarea></td>
-      
-           </tr>
-          <tr>
-            <td><center><input type="submit" value="Send" /></center></td>
-          </tr>
-        </table>
-        </form>
-        </td>
-      </tr>
-    </table>
-    </td>
-  </tr>
-</table>
-
-</body>
-
->>>>>>> 6f6f55a882e0bb2592c0a1d2a64cee1c1b9ba667
-</html>
