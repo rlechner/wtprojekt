e317ed0b99a038819b064486433018ce125a2765
rlechner <robertlechner@gmx.de>
2012-12-28 01:29:29 +0100

    kleine Anpassungen

--- dictro/application/DBSettings.php
+++ dictro/application/DBSettings.php
@@ -1,8 +1,8 @@
 <?php
 class DBSettings {
     const HOST   = 'localhost';
-    const SCHEMA = 'wbuch';
-    const USER   = 'diablo';
-    const PASSWD = 'diablo';
+    const SCHEMA = 'dictro';
+    const USER   = 'dictro';
+    const PASSWD = 'Lb6hc5stNVWdq8J4';
 
 }
