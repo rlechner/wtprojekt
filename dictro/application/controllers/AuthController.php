891cfe1c28fdc210970a07b6d8929e050e830b07
swimmer <pape.pula@googlemail.com>
2013-01-10 19:59:44 +0100

    css

--- dictro/application/controllers/AuthController.php
+++ dictro/application/controllers/AuthController.php
@@ -1,55 +1,49 @@
 <?php
 
-class AuthController extends Zend_Controller_Action
-{
- 
-    public function loginAction()
-    {
-        $db = $this->_getParam('db');
- 
-        $loginForm = new Application_forms_Auth_Login($_POST);
- 
-        if ($loginForm->isValid()) {
- 
-            $adapter = new Zend_Auth_Adapter_DbTable(
-                $db,
-                'users',
-                'name',
-                'password'
-                );
- 
-            $adapter->setIdentity($loginForm->getValue('username'));
-            $adapter->setCredential($loginForm->getValue('password'));
- 
-            $result = $auth->authenticate($adapter);
- 
-            if ($result->isValid()) {
-                $this->_helper->FlashMessenger('Erfolgreich angemeldet');
-                $this->redirect('/');
-                return;
-            }
- 
-        }
- 
-        $this->view->loginForm = $loginForm;
- 
-    }
- 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-        // action body
+	class AuthController extends Zend_Controller_Action
+	{
 		
-    }
-  
+		public function init(){
+				
+			/* Initialize action controller here */
+				
+		}
 	
-}
+		public function indexAction(){
+		/*	
+			$db = Zend_Registry::get('dbc');
+			
+			$loginForm = new Application_Form_Login();
+			
 
-?>
-
-
+			if ($loginForm->isValid()) {
+					
+				$adapter = new Zend_Auth_Adapter_DbTable(
+						$db,
+						'users',
+						'name',
+						'password'
+				);
+					
+				$adapter->setIdentity($loginForm->getValue('name'));
+				$adapter->setCredential($loginForm->getValue('password'));
+					
+				$result = $auth->authenticate($adapter);
+					
+				if ($result->isValid()) {
+					$this->_helper->FlashMessenger('Erfolgreich angemeldet');
+					$this->redirect('/');
+					return;
+				}
+					
+			}
+			
+			$this->view->loginForm = $loginForm;
+	*/	
+		}
+		
+	
+	
+	
+	}
 
