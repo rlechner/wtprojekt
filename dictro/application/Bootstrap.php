68ad9d1096a070a947a92ecb53c4b2224b4cc633
rlechner <robertlechner@gmx.de>
2013-01-10 06:23:10 +0100

    Autoloading implementiert, VocabelDatenAdapter implementiert, VocabelDatenMapper Implementiert, usw

--- dictro/application/Bootstrap.php
+++ dictro/application/Bootstrap.php
@@ -10,45 +10,38 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		require_once 'Zend/Db.php';
 		require_once 'Zend/Db/Table.php';
 		require_once 'Zend/Debug.php';
-		require_once '../application/models/VocableModel.php';
+		require_once '../application/models/Vocable.php';
 		
 		parent::_bootstrap($resource);
 		
-
-		require_once "Zend/Loader.php";
+		
+		require_once 'Zend/Loader/Autoloader.php';
 		
 		//-- Set up Autoload
-		Zend_Loader_Autoloader::registerAutoload();
+		   
+    	Zend_Loader_Autoloader::getInstance();
 		
 		
 		$mysqli = new mysqli(DBSettings::HOST, DBSettings::USER, DBSettings::PASSWD, DBSettings::SCHEMA);
-			
+		
 		if (!$mysqli) {
-			printf("Connect failed: %s\n", mysqli_connect_error());
-			exit();
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
 		}
 		
 		$mysqli->set_charset('utf8');
 		Zend_Registry::set('dbc', $mysqli);
-	
-
-		$params = array ('host'     => DBSettings::HOST,
+		
+		
+		$params = array ('host' => DBSettings::HOST,
 						'username' => DBSettings::USER,
 						'password' => DBSettings::PASSWD,
-						'dbname'   => DBSettings::SCHEMA);
-				
+						'dbname' => DBSettings::SCHEMA);
+		
 		$db = Zend_Db::factory('PDO_MYSQL', $params);
 		Zend_Db_Table::setDefaultAdapter($db);
 
 	}
-		
-	protected function _initDoctype()
-	{
-		$this->bootstrap('view');
-		$view = $this->getResource('view');
-		$view->doctype('HTML4_STRICT');
-	}
-	
 	
 	
 }
--- dictro/application/controllers/TranslationController.php
+++ dictro/application/controllers/TranslationController.php
@@ -6,41 +6,35 @@ class TranslationController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+
     }
 
     public function indexAction()
     {
         $request = $this->getRequest();
 		$form = new Application_Form_Translate();
-
+		
 		if ($this->getRequest()->isPost()) {
-			$this->request =  $this->getRequest();
+			$this->request = $this->getRequest();
 			$x = $this->request->getParam('vocable');
 			var_dump($x);
 			if ($form->isValid($this->request->getPost())) {
-				print_r("ist Valide");
 				$db = Zend_Registry::get('dbc');
-				
+			
 				if (! is_null($db)) {
 					$values = $form->getValues();
-					$translation = $db->query('SELECT german, english from vocabulary WHERE german LIKE "%' . $values['vocable'] . '%";');
+					$translation = $db->query('SELECT german, english from vocable WHERE german LIKE "%' . $values['vocable'] . '%";');
 					$wert = mysqli_fetch_assoc($translation);
+					print_r($wert);
 					$this->view->translation = $wert;
 					//return $this->_helper->redirector('index');
-				}
+					}
 			}
 		}
 		$this->view->form = $form;
 		
     }
     
-    public function showAction()
-    {
-    	$vocable = new Application_Model_VocableMapper();
-    	$this->view->row = $vocable->fetchRow('voc_id = 1');
-    }
-    
-
 
 }
 
