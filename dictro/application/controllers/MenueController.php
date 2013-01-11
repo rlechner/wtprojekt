8cfa986c9099c2ebeec04cf5864b3109e562c46a
swimmer <pape.pula@googlemail.com>
2013-01-11 22:22:02 +0100

    login

--- dictro/application/controllers/MenueController.php
+++ dictro/application/controllers/MenueController.php
@@ -19,58 +19,65 @@ class MenueController extends Zend_Controller_Action
 		$loginForm = new Application_Form_Login($_POST);
 		
 
-	if ($this->getRequest()->isPost()) {
-		$this->request = $this->getRequest();
-		if (isset($_POST['submit']) && $loginForm->isValid($_POST)) {
-
-		$adapter = new Zend_Auth_Adapter_DbTable(
-		$db,
-		'users',
-		'name',
-		'password'
-		);
-
-		$adapter->setIdentity($loginForm->getValue('username'));
-		$adapter->setCredential($loginForm->getValue('password'));
-
-		$result = $adapter->authenticate($adapter);
-
-
-		if ($result->isValid()) {
-			
-			//new Zend_Session_Namespace();
-			
-			//$this->_helper->FlashMessenger('Erfolgreich angemeldet');
-			
-			
-			
-			new Zend_Session_Namespace($loginForm->getValue('username'));
-			
-			$this->redirect('index');
-
-			echo "Anmeldung erfolgreich";
-			
-			
-			
-			
-			//Zend_Session::start();
-			//new Zend_Session_Namespace();
-			//session_start();
-			
-			return;
-		}
-		else {
-			
-			echo "Wrong Username or Password";
-			
-		}
-	}
+		if ($this->getRequest()->isPost()) {
+			$this->request = $this->getRequest();
+			if (isset($_POST['submit']) && $loginForm->isValid($_POST)) {
+	
+			$adapter = new Zend_Auth_Adapter_DbTable(
+			$db,
+			'users',
+			'name',
+			'password'
+			);
+	
+			$adapter->setIdentity($loginForm->getValue('username'));
+			$adapter->setCredential($loginForm->getValue('password'));
+	
+			$result = $adapter->authenticate($adapter);
 	
 	
-	}
+				if ($result->isValid()) {
+					
+					//new Zend_Session_Namespace();
+					
+					//$this->_helper->FlashMessenger('Erfolgreich angemeldet');
+					
+					//var_dump ($result);
+
+					new Zend_Session_Namespace($loginForm->getValue('username'));
+
+					echo "Anmeldung erfolgreich";
+					
+					$vision=1;
+						
+					$this->view->vision = $vision;
+					
+					$loginForm=null;
+
+					//$this->redirect('index');
+					
+					//Zend_Session::start();
+					//new Zend_Session_Namespace();
+					//session_start();
+					
+					return;
+				}
+				else {
+					
+					echo "Wrong Username or Password";
+					
+				}
+			}
+		}
+		
 	$this->view->loginForm = $loginForm;
 	
 	}
 
 }
+
+
+
+
+
 ?>
