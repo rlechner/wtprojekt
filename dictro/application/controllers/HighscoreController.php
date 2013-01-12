76a875c04ead62d0c259ff3c8d86010e118171e2
swimmer <pape.pula@googlemail.com>
2013-01-12 15:38:23 +0100

    highscore failed

--- dictro/application/controllers/HighscoreController.php
+++ dictro/application/controllers/HighscoreController.php
@@ -10,9 +10,52 @@ class HighscoreController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
-    }
+    	$request = $this->getRequest();
+    	
+    	$result1 = "";
+    	$result2 = "";
+    	 
+    	if ($this->getRequest()->isPost()) {
+    		$this->request = $this->getRequest();
+    		if ($form->isValid($this->request->getPost())) {
+    			$db = Zend_Registry::get('dbc');
+    			$db->query('SET NAMES utf8;');
+    	
+    			if (! is_null($db)) {
+
+    					$stmt = $db->prepare(
+    							'SELECT
+	                                		user_id, score
+	                      		FROM
+	                                		highscore
+    	
+	                     		ORDER BY 	score;');
+    						
+    					$stmt->execute();
+    					$stmt->bind_result($result1, $result2);
+    				}
+    	
+    				$ergebnis = array();
+    				$i = 0;
+    				// Array ausgeben
+    				while($stmt->fetch()) {
+    					$ergebnis[$i][0]=$result1;
+    					$ergebnis[$i][1]=$result2;
+    					$i++;
+    				}
+    				$stmt->close();
+    				 
+    				$this->view->highscore = $ergebnis;
+    				//return $this->_helper->redirector('index');
+    			}
+    		}
+    	}
+    	
+    
 	
+    
+    
+    
 	public function highscoreresetAction(){
 	
 	}
