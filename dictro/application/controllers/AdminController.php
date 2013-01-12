893f0e7b72e65abfc86cc7c45ee15136cefb23a1
rlechner <robertlechner@gmx.de>
2013-01-12 22:46:55 +0100

    DeleteVocabelAdmin implementiert

--- dictro/application/controllers/AdminController.php
+++ dictro/application/controllers/AdminController.php
@@ -18,6 +18,88 @@ class AdminController extends Zend_Controller_Action
 	}
 
 	public function deletevocableAction(){
+		
+		
+		$request = $this->getRequest();
+		$formSearch = new Application_Form_SearchVocableAdmin();
+		$result1 = "";
+		$result2 = "";
+		$result3 = "";
+		 
+		if ($this->getRequest()->isPost()) {
+			$this->request = $this->getRequest();
+			if (isset($_POST['search_button']) && $formSearch->isValid($this->request->getPost())) {
+				$db = Zend_Registry::get('dbc');
+				$db->query('SET NAMES utf8;');
+		
+				if (! is_null($db)) {
+					$values = $formSearch->getValues();
+					// 					$translation = $db->query('SELECT german, english from vocable WHERE german LIKE "%' . $values['vocable'] . '%";');
+					// 					$wert = mysqli_fetch_assoc($translation);
+					$stmt = $db->prepare(
+		    						'SELECT
+		                                		voc_id, german, english
+		                      		FROM
+		                                		VOCABLE
+					
+		                     		WHERE 		german LIKE "' . $values['search_voc'] . '%";');
+		    	
+		    				$stmt->execute();
+					$stmt->bind_result($result1, $result2, $result3);
+		
+					$ergebnis = array();
+					$i = 0;
+					// Array ausgeben
+					while($stmt->fetch()) {
+						$ergebnis[$i][0]=$result1;
+						$ergebnis[$i][1]=$result2;
+						$ergebnis[$i][2]=$result3;
+						$i++;
+					}
+					$stmt->close();
+					 
+					$this->view->admin = $ergebnis;
+					//return $this->_helper->redirector('index');
+				}
+			}
+		}
+		 
+		$this->view->formSearch = $formSearch;
+		
+		$formDelete = new Application_Form_DeleteVocabelAdmin();
+		
+		if ($this->getRequest()->isPost()) {
+			$this->request = $this->getRequest();
+			if (isset($_POST['delete_button']) && $formDelete->isValid($this->request->getPost())) {
+				$db = Zend_Registry::get('dbc');
+				if (! is_null($db)) {
+					$values = $formDelete->getValues();
+		
+					$stmt = $db->prepare(
+		    						'DELETE
+		                                		*
+		                      		FROM
+		                                		VOCABLE
+					
+		                     		WHERE 		voc_id = "' . $values['voc_ID'] . '";');
+		    	
+		    				$stmt->execute();
+		
+					$this->view->deleteerfolgreich = 'Vocable: "' .  $values['voc_ID'] . '" successful deleted';
+					/*
+					 }
+					else {
+		
+		
+					echo "Registrierung failed";
+		
+					}*/
+		
+				}
+			}
+		}
+		$this->view->formDelete = $formDelete;
+		
 	}
 	
 	public function updatevocableAction(){
