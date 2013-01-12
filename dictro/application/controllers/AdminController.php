c22e1572ed2f50412b6ab1ec31c0636ccac58f38
unknown <gloa.gruszki@googlemail.com>
2013-01-12 17:21:27 +0100

    arbeit am admincont

--- dictro/application/controllers/AdminController.php
+++ dictro/application/controllers/AdminController.php
@@ -16,33 +16,35 @@ class AdminController extends Zend_Controller_Action
 	public function searchvocableAction(){
 		
 		$request = $this->getRequest();
-		$result1 = "";
+    	$form = new Application_Form_Search_Vocable_Admin($_POST);
+    	$result1 = "";
     	$result2 = "";
-		
-		if ($this->getRequest()->isPost()) {
-    		$this->request = $this->getRequest();
-			
-			if (isset($_POST['submit'])){
+    	
+    	if ($this->getRequest()->isPost()) {
+			$this->request = $this->getRequest();
+			if (isset($_POST['submit']) && $form->isValid($_POST)) {
     			$db = Zend_Registry::get('dbc');
     			$db->query('SET NAMES utf8;');
-				
-				if (! is_null($db)) {
-					
+    				
+    			if (! is_null($db)) {
+    				$values = $form->getValues();
+    				// 					$translation = $db->query('SELECT german, english from vocable WHERE german LIKE "%' . $values['vocable'] . '%";');
+    				// 					$wert = mysqli_fetch_assoc($translation);
 					$stmt = $db->prepare(
 		    						'SELECT
 		                                		german, english
 		                      		FROM
 		                                		VOCABLE
 					
-		                     		WHERE 		german LIKE "' . $request->search_admin . '%";');
-									
-					$stmt->execute();
-		    		$stmt->bind_result($result1, $result2);
-					
-					$ergebnis = array();
+		                     		WHERE 		german LIKE "' . $values['search_voc'] . '%";');
+		    	
+		    				$stmt->execute();
+		    				$stmt->bind_result($result1, $result2);
+    				
+    				$ergebnis = array();
     				$i = 0;
-					
-					while($stmt->fetch()) {
+    				// Array ausgeben
+    				while($stmt->fetch()) {
     					$ergebnis[$i][0]=$result1;
     					$ergebnis[$i][1]=$result2;
     					$i++;
@@ -50,10 +52,12 @@ class AdminController extends Zend_Controller_Action
     				$stmt->close();
     	
     				$this->view->admin = $ergebnis;
-				}
-			}
-		}
-	}
+    				//return $this->_helper->redirector('index');
+    			}
+    		}
+    	}
+    	$this->view->form = $form;
+    }
 
 	public function deletevocableAction(){
 	}
